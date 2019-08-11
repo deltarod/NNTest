d0e578fa8487233abc18dd2a3222213b2af49252
Tristan Miller <tem83@nau.edu>
2019-08-10 18:07:35 -0700

    Yes

--- src/NeuralNetwork.java
+++ src/NeuralNetwork.java
@@ -51,8 +51,9 @@ public class NeuralNetwork
             {
                 n.prevLayerSig = new Matrix( 1, prevLayerSize, Matrix.SD );
 
-
             }
+
+
         }
 
         Matrix calculateOutput( Matrix input )
@@ -89,19 +90,50 @@ public class NeuralNetwork
             }
             else
             {
-                return nextLayer.calculateOutput();
+                return nextLayer.calculateOutput( layerOutputMatrix );
             }
         }
     }
 
+    //TODO: implement breeding and/or asexual reproduction
 
-    NeuralNetwork(int[] layerSizes, int numLayers, int sigType )
+    NeuralNetwork breed( NeuralNetwork partner )
+    {
+        NeuralNetwork child = new NeuralNetwork( layers, numLayers, sigType );
+
+        child.generate();
+
+        Layer layer = head, other = partner.head, childHead = child.head;
+
+
+    }
+
+    NeuralNetwork( int[] layerSizes, int numLayers, int sigType )
     {
         layers = layerSizes;
 
         this.numLayers = numLayers;
 
         this.sigType = sigType;
+    }
+
+    private void generate()
+    {
+        int layerIncrement;
+
+        Layer layer, head;
+
+        //layer 0 is input layer, layer 1 is first layer of perceptrons
+        head = new Layer(layers[ 0 ], layers[ 1 ] );
+
+        layer = head;
+
+        for( layerIncrement = 2; layerIncrement < numLayers; layerIncrement++ )
+        {
+            layer.nextLayer = new Layer( layer.numNeurons, layers[ layerIncrement] );
+
+            layer.nextLayer.prevLayer = layer;
+        }
     }
 
 
